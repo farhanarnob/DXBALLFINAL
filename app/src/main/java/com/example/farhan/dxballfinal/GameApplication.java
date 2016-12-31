f91bd963228e71e1aba078b567dc163079aee94f
Farhan Rahman Arnob <farhanarnob@live.com>
2016-12-31 15:43:57 +0600

    application redBrush

--- app/src/main/java/com/example/farhan/dxballfinal/GameApplication.java
+++ app/src/main/java/com/example/farhan/dxballfinal/GameApplication.java
@@ -1,10 +1,24 @@
 package com.example.farhan.dxballfinal;
 
 import android.app.Application;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 /**
  * Created by ${farhanarnob} on ${06-Oct-16}.
  */
 
 public class GameApplication extends Application {
+    Paint redBrush;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        redBrush = new Paint();
+        redBrush.setColor(Color.RED);
+        redBrush.setStyle(Paint.Style.FILL);
+    }
+    public Paint getRedBrush(){
+        return redBrush;
+    }
 }
